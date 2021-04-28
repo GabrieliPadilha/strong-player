8082fb588e5f592eeaaa958e67ba99f5b1e3ebb1
Gabrieli Padilha <gabrielip.souza@hotmail.com>
2021-04-27 21:35:05 -0300

    player

---
+++ data.js
@@ -0,0 +1,20 @@
+window.audios = [
+    {
+        title: 'CB13 Me esquece',
+        artist: 'CB13',
+        cover: 'img.jpg',
+        file: 'cb13-me-esquece.mp3'
+    },
+    {
+        title: 'konai Moça',
+        artist: 'Kanai',
+        cover: 'img1.jpg',
+        file: 'konai-moça.mp3'
+    },
+    {
+        title: 'If You want',
+        artist: 'NF',
+        cover: 'img2.jpg',
+        file: 'NF-If You Want.mp3'
+    }
+]
